66751d8a8befac61f5dd263fb0a97ec62e6aaf61
s2010 <bashayer.hijji@gmail.com>
2016-12-26 08:54:19 +0300

    added The task manager

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <?php
 
+
 use Todo\Models\Task;
 use Todo\TaskManager;
 use Todo\Storage\DatabaseTaskStorage;
 
 
-session_start();
 require_once  'vendor/autoload.php';
 
 //db connection
-
 $db = new PDO ('mysql:host=127.0.0.1;dbname=todo', 'root', 'root');
+$storage = new DatabaseTaskStorage($db);
+$manager = new TaskManager($storage);
 
-$manager = new TaskManager(new DatabaseTaskStorage($db));
+
 $task = new Task();
-$task->setDescription('Learn To Believe in what you build and Have Fun');
+$task->setDescription('A new Task');
+$task->setDue(new DateTime);
+
+$storedTask = $manager->addTask($task);
+//you can redirect using $storedTask->getId(); and show Task based on id
